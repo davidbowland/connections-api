aae521c4338ab1a2dec009dbcdcf6009a0a77f29
David Bowland <dbowland1@gmail.com>
2025-08-02 10:24:06 -0500

    Adjust how inspiration is used

--- jest.setup-test-env.js
+++ jest.setup-test-env.js
@@ -15,10 +15,10 @@ process.env.LLM_PROMPT_ID = 'create-connections-game'
 
 process.env.AVOID_NEXT_GAMES_COUNT = '10'
 process.env.AVOID_PAST_GAMES_COUNT = '20'
-process.env.INSPIRATION_ADJECTIVES_COUNT = '5'
-process.env.INSPIRATION_NOUNS_COUNT = '20'
-process.env.INSPIRATION_TIME_PERIODS_COUNT = '3'
-process.env.INSPIRATION_VERBS_COUNT = '10'
+process.env.INSPIRATION_ADJECTIVES_COUNT = '2'
+process.env.INSPIRATION_NOUNS_COUNT = '2'
+process.env.INSPIRATION_TIME_PERIODS_COUNT = '2'
+process.env.INSPIRATION_VERBS_COUNT = '2'
 
 // Logging
 
